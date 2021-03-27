556b533779eef58ee7cf1a15c6da6c22ce277a30
timur <matskevich-06@mail.ru>
2021-03-27 10:05:54 +0400

    variables type

--- calc.py
+++ calc.py
@@ -1,3 +1,5 @@
-number = 5
-fnumber = 5.5
-name = 'timur'
+number = 5 #integer (int)
+fnumber = 5.5 #float (float)
+name = 'timur' #string (str)
+status = True #boolean (bool)
+status2 = False #another bool
