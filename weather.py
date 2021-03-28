5a4d4a033ef1c8b331a53726da9072e32ea71c9d
timur <matskevich-06@mail.ru>
2021-03-28 21:23:59 +0400

    outputting info

--- weather.py
+++ weather.py
@@ -9,3 +9,5 @@ observation = api_key.weather_at_place(place) #Serching information
 weather_info = observation.get_weather() #Highlighting weather from data
 temp_info = weather_info.get_temperature('celsius')['temp'] #Getting temperature
 detailed_status = weather_info.get_detailed_status() #Getting sky status
+print('The temperature is ', temp_info, ' degrees Celsius', sep = '') #Outputting temperature
+print('The status is ', detailed_status, sep = '') #Outputting sky status
