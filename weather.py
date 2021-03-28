92ee0b520ccab6266a786c4f1f0a941fb10a9fb0
timur <matskevich-06@mail.ru>
2021-03-28 21:23:11 +0400

    highlighting temperature and sky status

--- weather.py
+++ weather.py
@@ -7,3 +7,5 @@ api_key = pyowm.OWM('26177363c268ee1d29d88a3aa230a3d8') #Getting api key
 place = input('Enter place to get weather information: ') #Getting place
 observation = api_key.weather_at_place(place) #Serching information
 weather_info = observation.get_weather() #Highlighting weather from data
+temp_info = weather_info.get_temperature('celsius')['temp'] #Getting temperature
+detailed_status = weather_info.get_detailed_status() #Getting sky status
