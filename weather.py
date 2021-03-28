b7754f03b74e8ed146ef4916d300575d5c6c158e
timur <matskevich-06@mail.ru>
2021-03-28 21:20:16 +0400

    importing necessary modules

---
+++ weather.py
@@ -0,0 +1,4 @@
+import pyowm #Importing necessary module
+#documentation and Youtube guides)
+from colorama import init #importing a package for Windows
+from colorama import Fore, Back, Style #importing necessary packages
