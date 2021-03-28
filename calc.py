c891f4c301fbd22e0cec0688aef61f56ea52f75f
timur <matskevich-06@mail.ru>
2021-03-28 07:38:21 +0400

    summing

--- calc.py
+++ calc.py
@@ -3,4 +3,6 @@ operation = input('Select the operation(+/-): ') #selecting the operation
 if operation == '+' or operation == '-': #check if the operation symbol is
     #correct
     num1 = float(input('Enter first number: ')) #if yes, entering numbers
-    num2 = float(input('Enter second number: ')) #
+    num2 = float(input('Enter second number: ')) #second input()
+    if operation == '+': #summing operations
+        
